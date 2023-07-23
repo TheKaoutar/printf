6820bcdf84f04ca45034ba37cf34325f4e3663ab
TheKaoutar <kaoutar.bakri7@gmail.com>
2023-07-23 12:48:44 -0700

    main.h commit

--- main.h
+++ main.h
@@ -1,9 +1,12 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
 #include <stdlib.h>
+
 int _printf(const char *format, ...);
+
 #endif
